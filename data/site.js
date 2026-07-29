dcedf66ba43885305f1a1ad14697c34219161ac7
idatsai <mydearrm@gmail.com>
2026-07-30 00:45:27 +0800

    Update site.js

--- data/site.js
+++ data/site.js
@@ -2,7 +2,7 @@ window.RHB_SITE = {
   title: "2026 International Forum on Taiwan Cultural Heritage",
   intro: "Seeing Taiwan from a Global Perspective, Preserving Heritage for the Future.",
   footer: {
-    label: "Switzerland．鐵道國際交流",
+    label: "BUREAU OF CULTURAL HERITAGE, MINISTRY OF CULTURE, TAIWAN",
     title: "2026 International Forum on Taiwan Cultural Heritage",
     copyright: "易圖科技股份有限公司 版權所有 2026 © EasyMap Digital Technology Inc. All Rights Reserved."
   },
